a765209f686f3038d329ef7026dafbb81907d906
박기림 <zzrlfla@naver.com>
2022-10-03 00:02:13 +0900

    큐 구현 & 스토리 배치 준비

--- Assets/Scripts/select.cs
+++ Assets/Scripts/select.cs
@@ -6,13 +6,11 @@ using UnityEngine.EventSystems;
 public class select : MonoBehaviour, IPointerClickHandler
 {
     //  타 스크립트에서 chatBox 끄기, Panel 켜기하며 호출됨
-    mainButton mClass = new mainButton();
-    public Queue<int> selectQ;
-
+    public Queue selectQ = selectQClass.selectQ;
 
     void Start()
     {
-        selectQ = mClass.selectQ;
+
     }
 
 
@@ -27,7 +25,10 @@ public class select : MonoBehaviour, IPointerClickHandler
             selectQ.Enqueue(2);
         }
 
-        Debug.Log(selectQ.Dequeue());
+        Debug.Log("큐 사이즈 : "+ selectQ.Count);
+
+        
+        
     }
 
 }
