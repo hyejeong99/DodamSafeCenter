2dce58e8d1fc8fcf7a3fb5e6ee15ccfd7e74d635
hyejeong99 <g2040g@naver.com>
2022-11-08 16:05:44 +0900

    배경음악 수정

--- Assets/Scripts/select.cs
+++ Assets/Scripts/select.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class select : MonoBehaviour, IPointerClickHandler
 {
     //  타 스크립트에서 chatBox 끄기, Panel 켜기하며 호출됨
     public Queue selectQ = selectQClass.selectQ;
     GameObject nextButton;
+    public GameObject umbrellaBG, raincoatBG;
 
     void Start()
     {
@@ -22,10 +24,20 @@ public class select : MonoBehaviour, IPointerClickHandler
         {
             selectQ.Enqueue(0);
             PlayerPrefs.SetInt("select", 0);
+            if(umbrellaBG!=null){
+                if(SceneManager.GetActiveScene().name=="SummerStoryScene"){//우산
+                    umbrellaBG.SetActive(true);
+                }
+            }
         }
         else if (this.tag == "select2")
         {
             selectQ.Enqueue(1);
+            if(raincoatBG!=null){
+                if(SceneManager.GetActiveScene().name=="SummerStoryScene"){//우비
+                    raincoatBG.SetActive(true);
+                }
+            }
         }
 
         nextButton.GetComponent<Image>().enabled = true;
